8e92b3f15e1c37ef2e09fc9e3e182ef5ed29f782
internetwarrior <kairatbektov@gmail.com>
2025-06-11 23:26:21 +0600

    update 0.2

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ let element_2 = document.querySelector(".form-row");
 let element_3 = document.querySelector(".action");
 let element_7 = document.querySelector(".auto-p");
 let fallButton =
-  '<button type="button" class="button emoji" onclick="let n=this.innerText.split(`-`)[1]; if(n<4)this.innerText=`⚠️-${++n}`">⚠️-0</button>';
+  '<button style="cursor:pointer;" type="button" class="button emoji" onclick="let n=this.innerText.split(`-`)[1]; if(n<4)this.innerText=`⚠️-${++n}`">⚠️-0</button>';
 
 let warnButton =
-  '<button type="button" class="button emoji" onclick="let n=this.innerText.split(`-`)[1]; if(n<2)this.innerText=`📛-${++n}`">📛-0</button>';
+  '<button style="cursor:pointer;" type="button" class="button emoji" onclick="let n=this.innerText.split(`-`)[1]; if(n<2)this.innerText=`📛-${++n}`">📛-0</button>';
 
 document.querySelector(".action").onclick = function () {
   if (document.querySelector(".action").innerHTML == "Подтвердить") {
@@ -19,19 +19,10 @@ document.querySelector(".action").onclick = function () {
   }
 };
 
-document.querySelector(".button-action").onclick = function () {
-  if (document.querySelector(".button-action").innerHTML == "Вернуться назад") {
-    document.querySelector(".auto-p").innerHTML = "";
-    function_hiden_1();
-  } else {
-    function_hiden_2();
-  }
-};
-
 function auto() {
   let index_rol = 0;
   let kolichestvo_user = document.querySelector(".user-form").value || 10;
-
+  let final_roles = [];
   if (kolichestvo_user > 30) {
     alert("Не вводите так много!");
   } else {
@@ -42,10 +33,13 @@ function auto() {
       let area_rand = [];
       let i = 1;
       let i_new = 1;
-      let roles_massiv = ["Мафия", "Комиссар", "Доктор"];
+      let roles_massiv = ["Мафия", "Комиссар", "Доктор", "Дон"];
       let mafia = Math.floor(kolichestvo_user / 3);
 
+      //clear-roles
       document.querySelector(".auto-p").innerHTML = "";
+
+      //randomizing roles
       while (roles_massiv.length != mafia + 2) {
         roles_massiv.push("Мафия");
       }
@@ -70,28 +64,73 @@ function auto() {
         if (area_rand.indexOf(rand) == -1) {
           area_rand.push(rand);
         }
-
-        document.querySelector(".auto-p").innerHTML += `
-        <div style=''>
-      
-         ${i_new}  ${roles_massiv[rand]} 
-           <div >  ${fallButton} ${warnButton} <div/> 
-         <div/> 
-         <hr/>
-        `;
-
+        renderRoles(i_new, roles_massiv[rand]);
         i_new = i_new + 1;
+        final_roles.push(roles_massiv[rand]);
       }
+      localStorage.setItem("roles", JSON.stringify(final_roles));
+      console.log(final_roles);
     }
   }
+}
+
+function setRoles() {
+  console.log("set!");
+  const roles = JSON.parse(localStorage.getItem("roles")) || [];
+  roles.map((role, index) => {
+    renderRoles(index + 1, role);
+  });
+}
+
+setRoles();
+
+function renderRoles(number, role) {
+  document.querySelector(".auto-p").innerHTML += `
+        <div class='${
+          ["Мафия", "Комиссар", "Доктор", "Дон"].includes(role)
+            ? "active-role"
+            : ""
+        }'>
+          <div class='role-copy' onclick='copyRole(event)'>
+          ${number}  ${role} 
+          </div>
+
+           <div >  ${fallButton} ${warnButton} </div> 
+
+         </div> 
+         <hr/>
+        `;
+}
+
+function copyRole(event) {
+  if (event.target.innerText == "Скопировано!") {
+    return;
+  }
+  const role = event.target.innerText;
+  navigator.clipboard.writeText(role);
+  event.target.innerText = "Скопировано!";
+  setTimeout(() => {
+    event.target.innerText = role;
+  }, 2000);
 }
 
 // -----------------TIMER--------------
 
 function updateDisplay() {
-  const mins = String(Math.floor(seconds / 60)).padStart(2, "0");
-  const secs = String(seconds % 60).padStart(2, "0");
-  document.getElementById("timerDisplay").innerText = `${mins}:${secs}`;
+  const timerElement = document.getElementById("timerDisplay");
+  const mins = Math.floor(seconds / 60);
+
+  const secs = seconds % 60;
+
+  timerElement.innerText = `${String(mins).padStart(2, "0")}:${String(
+    secs
+  ).padStart(2, "0")}`;
+
+  if (mins == 0 && secs <= 10) {
+    timerElement.classList.add("timer-animation");
+  } else {
+    timerElement.classList.remove("timer-animation");
+  }
 }
 
 function timerLoop() {
